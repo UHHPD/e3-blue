22be1ab2c81aba1eeddea64af901bd097475a85a
tore1311 <tore@arch.localdomain>
2021-11-12 19:27:42 +0100

    fixes

--- likelihood.cc
+++ likelihood.cc
@@ -1,20 +1,61 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 
 double poisson(double mu, int k) {
-    return 0;
+    return pow(mu, k) * exp(-mu)/tgamma(k+1);
+}
+
+double prob(std::vector<int> daten, double mu) {
+    double L = 1;
+    for(int k : daten){
+        L *= poisson(mu, k); 
+    }
+    return L;
+}
+
+double nll(std::vector<int> daten, double mu) {
+    double L = 0;
+    for(int k : daten) {
+        L -= log(poisson(mu, k));
+    }
+    return L;
 }
 
 int main() {
+    // read in data
     using namespace std;
-
-
     ifstream fin("datensumme.txt");
+    std::vector<int> daten;
     int n_i;
-    for(int i = 0 ; i < 234 ; ++i) {
+    for(int i = 0; i < 234; i++) {
         fin >> n_i;
+        daten.push_back(n_i);
     }
     fin.close();
+
+    // Likelihood
+    double mu = 3.11538;
+    std::cout << prob(daten, mu) << std::endl;
+
+    // scanning likelihood
+    int start = 0;
+    int end = 6;
+    double step = 0.1;
+    ofstream fout("likelihood.txt");
+    for(mu = start; mu <= end; mu += step) {
+        fout << mu << " " << prob(daten, mu) << endl;
+    }
+    fout.close();
+    
+    // scanning log likelihood
+    ofstream fnll("nll.txt");
+    for(mu = start; mu <= end; mu += step) {
+        //fnll << mu << " " << nll(daten, mu) << endl;
+        fnll << mu << " " << -log(prob(daten, mu)) << endl;
+    }
+    fnll.close();
+
 }
--- poisson.cc
+++ poisson.cc
@@ -1,20 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 
 double poisson(double mu, int k) {
-    return 0;
+    return pow(mu, k) * exp(-mu) / tgamma(k+1);
 }
 
 int main() {
     using namespace std;
 
-
     ifstream fin("datensumme.txt");
     int n_i;
+    
+    vector<int> zaehler(11);
+    
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
+        zaehler[n_i]++;
     }
+
+    for(int k = 0 ; k < zaehler.size() ; ++k) {
+    	cout << k << " " << zaehler[k] << std::endl;
+    }
+    
+    
     fin.close();
+    ofstream fout("hist.txt"); 
+    ofstream poi_out("histpoi.txt");
+    double mu = 3.11538;
+    for(unsigned int k = 0 ; k < zaehler.size() ; ++k) {
+    	fout << k << " " << zaehler[k] << endl;
+        poi_out << k << " " << zaehler[k] << " " << poisson(mu, k) * 234 << endl; 
+    }
+    fout.close();
+    poi_out.close();
 }
